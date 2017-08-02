b13c33d316e9ab6db0af019a0d65a9928501c3b5
hunter <yunterakins@gmail.com>
2017-08-01 19:37:21 -0700

    created header file

---
+++ drum64.h
@@ -0,0 +1,50 @@
+
+
+typedef enum {
+	A;
+	B;
+	Z;
+	START;
+	UP;  //up on dpad
+	DOWN; //down on dpad
+	LEFT; //left on dpad
+	RIGHT;  //right on dpad
+	L; //left bumper
+	R; //right bumper
+	C-UP;
+	C-DOWN;
+	C-LEFT;
+	C-RIGHT;
+} buttons_t;
+
+
+typedef struct {
+	bool a;
+	bool b;
+	bool z;
+	bool start;
+	bool up;
+	bool down;
+	bool left;
+	bool right;
+	bool l; 
+	bool r;
+	bool c_up;
+	bool c_down;
+	bool c_left;
+	bool c_right;
+	char analogz;
+	char analogx;	
+} controller_t;
+
+
+// initialize GPIO pins for reading from shift register
+int Controller_Init();
+
+// the Poll function to write shift register vals into the controller struct
+int Poll(controller_t* controller);
+
+
+
+
+
