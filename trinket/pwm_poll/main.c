3651702b9118dca45898a13c9dc7ac1daf42df15
hunter <yunterakins@gmail.com>
2017-09-11 18:30:10 -0700

    polling

---
+++ trinket/pwm_poll/main.c
@@ -0,0 +1,73 @@
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+//pwm version of the poll
+// idea is to use pwm output
+// also use a separate timer interrupt that will change the duty cycle
+// basically, I have a 4us square
+// a 0 is represented as a 25% duty cycle, but starts delayed...kinda tricky.
+// a 1 is represented as a 75% duty cycle
+// the polling signal is a  
+
+volatile uint8_t tick;
+
+
+void timer0_init() {
+	/* Note about the control registers:
+	CTC functionality is distributed across the two control registers.
+	For example, to enable CTC, I need WGM00 = 0, WGM01 = 1, and WGM02 = 0
+	WGM02 is in register B, while WGM00 and WGM01 are in register A.
+	In this case, I only need to write to register A to enable CTC.
+	*/
+	
+	
+	// timer/counter control register A, used to set CTC (see pg. 79)
+	TCCR0A |= (1 << WGM01);
+
+	
+	// timer/counter control register B, prescaling the counter (see pg. 80)
+	TCCR0B |= (1 << CS00); // no prescalin
+		
+	// initialize timer to 0
+	TCNT0 = 0;
+	/* output compare register
+	whatever value is in TCNT0 gets compared to this value
+	at 8MHz, a tick is .125 us
+	8 ticks is 1 us
+	TCNT0 is going 0000, 0001, 0010, 0011, 0100, 0101, 0110, 0111, 1000;
+	*/
+	OCR0A = 8;	
+
+	// enable compare interrupt:
+	TIMSK |= (1 << OCIE0A);
+	// enable global interrupts
+
+	// initialize tick
+	tick = 0;
+	sei();
+
+}	
+
+
+
+ISR(TIMER0_COMPA_vect) {
+	PORTB ^= (1 << PB1);
+}
+
+int main(void)
+{
+
+	// set PB1 to output (red LED)
+	DDRB = (1 << DDB1);
+	PORTB |= (1 << PB1);
+
+	timer0_init();
+
+	while(1)
+	{
+	}
+}
+
+
